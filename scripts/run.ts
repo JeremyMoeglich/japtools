caee15f9db36c7d604906216370c5ae1bb02e426
Jeremy Möglich <jeremy.moeglich@gmail.com>
2022-09-16 21:50:24 +0200

    change prisma setup

--- scripts/run.ts
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config';
-import get_root from './get_root.js';
-import { cmd } from './script_util';
-
-const project_root = get_root();
-
-cmd(`cd ${project_root} && docker-compose up -d`);
