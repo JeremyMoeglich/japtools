ce6bb58d984b99ab9e316418c495ff6a24efcbd7
Jeremy Möglich <jeremy.moeglich@gmail.com>
2022-09-16 19:29:16 +0200

    add tauri

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,25 @@
 // @ts-check
 
-import adapter from '@sveltejs/adapter-auto';
+import staticAdapter from '@sveltejs/adapter-static';
+import nodeAdapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
+
+const hasAdapter = process.env.ADAPTER;
+const adapt = hasAdapter ? hasAdapter : 'node';
+
+const getAdapters = (adapt) => {
+	switch (adapt) {
+		case 'node':
+			return nodeAdapter;
+		case 'static':
+			return staticAdapter;
+		default:
+			console.log('unknown adapter, using node');
+			return nodeAdapter;
+	}
+};
+
+const adapter = getAdapters(adapt);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
