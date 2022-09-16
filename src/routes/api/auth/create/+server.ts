8929affb1db61ec7516d10dcbe0618051eec8e9a
Jeremy Möglich <jeremy.moeglich@gmail.com>
2022-09-16 21:55:54 +0200

    remove non js dependencies

--- src/routes/api/auth/create/+server.ts
+++ src/routes/api/auth/create/+server.ts
@@ -2,7 +2,7 @@ import { error, json } from '@sveltejs/kit';
 import { get_request_body } from '$lib/scripts/backend/endpoint_utils.server';
 import { prisma_client } from '$lib/scripts/backend/db/prisma_client.server';
 import type { RequestHandler } from './$types';
-import { hash } from 'bcrypt';
+import { hash } from 'bcryptjs';
 import cuid from 'cuid';
 import { z } from 'zod';
 
--- src/routes/api/auth/login/+server.ts
+++ src/routes/api/auth/login/+server.ts
@@ -2,7 +2,7 @@ import { json } from '@sveltejs/kit';
 import { get_request_body } from '$lib/scripts/backend/endpoint_utils.server';
 import { prisma_client } from '$lib/scripts/backend/db/prisma_client.server';
 import type { RequestHandler } from './$types';
-import { compare } from 'bcrypt';
+import { compare } from 'bcryptjs';
 import cuid from 'cuid';
 import { z } from 'zod';
 
