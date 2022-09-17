c9f8ac25aba43643a01b6a7b1032550d4060a163
Jeremy Möglich <jeremy.moeglich@gmail.com>
2022-09-17 11:19:19 +0200

    changes

--- src/lib/scripts/backend/db/prisma_client.server.ts
+++ src/lib/scripts/backend/db/prisma_client.server.ts
@@ -1,3 +1,7 @@
-import { PrismaClient } from '@prisma/client/edge';
+import { dev } from '$app/environment';
+
+const PrismaClient = dev
+	? await import('@prisma/client').then((m) => m.PrismaClient)
+	: await import('@prisma/client/edge').then((m) => m.PrismaClient);
 
 export const prisma_client = new PrismaClient();
